65c13b5a1bb4ffd9da453c2ae20e7dc5175fa1d7
Kedree <57106518+Kedree@users.noreply.github.com>
2022-08-03 17:21:03 -0400

    Add files via upload

--- HW6/HW6-2DONE.r
+++ HW6/HW6-2DONE.r
@@ -1,53 +1,28 @@
----
-title: "HW6-2DONE"
-author: "Kedree Proffitt"
-date: "2022-08-03"
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
-# a)
-
-## Imports
-
-```{r}
 library(ggplot2)
 library(dplyr)
 library(tidyverse)
 library(lubridate)
-```
 
-## Importing Data
+setwd("C:/Users/Kedree Proffitt/Downloads")
 
-```{r}
 load(file='house_prices.rda')
-```
 
-## Making the Plot
-
-```{r}
 ggplot(house_prices, aes(date, house_price_index)) +
 geom_line() +
 scale_x_continuous(name = "Date", breaks = c(ymd("1980-01-01"), ymd("2000-01-01"), ymd("2020-01-01")), labels = c("'80", "'00", "'20")) +
 scale_y_continuous(name = "House Price Index") +
 facet_wrap(vars(state), nrow=3, ncol=17)
-```
+  
+##############################
 
-# b)
-
-## Reshaping Data
-
-```{r}
 house_reshaped <- house_prices  %>% select(-house_price_perc)
 house_reshaped <- house_reshaped %>% gather(key="Measure", value="Value", house_price_index, unemploy_perc)
 
-## Plotting Data
 ggplot(house_reshaped, aes(color=Measure)) +
   geom_line(aes(date, Value)) +
   scale_x_continuous(name = "Date", breaks = c(ymd("1980-01-01"), ymd("2000-01-01"), ymd("2020-01-01")), labels = c("'80", "'00", "'20")) +
   scale_y_continuous(name = "House Price Index") +
   facet_wrap(vars(state), nrow=3, ncol=17)
-```
+
+#geom_line(aes(filter(house_reshaped, Measure=="house_price_index")$date, filter(house_reshaped, Measure=="house_price_index")$Value)) +
+#geom_line(aes(filter(house_reshaped, Measure=="unemploy_perc")$date, filter(house_reshaped, Measure=="unemploy_perc")$Value)) +
